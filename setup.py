2d967e24922b49306b91188a3c7879a8483f9729
Tod Schmidt <tschmidty@yahoo.com>
2018-01-31 18:53:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snipsmopidy',
-    version='1.0.0',
+    version='1.0.1',
     description='Mopidy skill for Snips',
     author='The Als',
     url='https://github.com/snipsco/snips-skill-mopidy',
